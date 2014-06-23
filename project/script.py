5c09f3d299c8a10c93cc8495b920efd1818926c8
w0bbe <skumtomten88@hotmail.com>
2014-06-23 02:01:36 +0200

    modified

--- project/script.py
+++ project/script.py
@@ -1,13 +1,35 @@
+import datetime
 import urllib
+import ephem
+
+
+
+print "\n *** Track Sat 0.1 *** \n"
 
 print "Downloading data"
 sock = urllib.urlopen("http://www.celestrak.com/NORAD/elements/stations.txt")
 htmlSource = sock.read()
 sock.close()
 
-print "Writing to file"
+print "Writing to file \n"
 logfile = open('rawdata', 'w')
 logfile.write(htmlSource)
 logfile.close()
 
-print "All done!"
+f = open('rawdata')
+datarows = f.readlines()
+datarows = [line.strip() for line in open('rawdata')]
+f.close()
+
+
+print "Information regarding : ", datarows[0]
+print datarows[1]
+print datarows[2]
+
+tle_rec = ephem.readtle(datarows[0], datarows[1], datarows[2])
+tle_rec.compute()
+
+print "Position: ", tle_rec.sublong, tle_rec.sublat
+
+
+print "\nAll done!"
