ebc19b4f42c69bd64e52c12b6b4fc7a72cc53b01
w0bbe <skumtomten88@hotmail.com>
2014-06-26 01:16:17 +0200

    added DMS to DD support

--- project/script.py
+++ project/script.py
@@ -7,14 +7,14 @@ import ephem
 print "\n *** Track Sat 0.1 *** \n"
 
 print "Downloading data"
-sock = urllib.urlopen("http://www.celestrak.com/NORAD/elements/stations.txt")
-htmlSource = sock.read()
-sock.close()
+#sock = urllib.urlopen("http://www.celestrak.com/NORAD/elements/stations.txt")
+#htmlSource = sock.read()
+#sock.close()
 
 print "Writing to file \n"
-logfile = open('rawdata', 'w')
-logfile.write(htmlSource)
-logfile.close()
+#logfile = open('rawdata', 'w')
+#logfile.write(htmlSource)
+#logfile.close()
 
 f = open('rawdata')
 datarows = f.readlines()
@@ -29,7 +29,25 @@ print datarows[2]
 tle_rec = ephem.readtle(datarows[0], datarows[1], datarows[2])
 tle_rec.compute()
 
+#Transform calc. latitude 
+DMSsublatS = "%s"%tle_rec.sublat
+DMSsublat = DMSsublatS.split(':')
+Dlat = int(DMSsublat[0])
+Mlat = int(DMSsublat[1])
+Slat = int(float(DMSsublat[2]))
+DMSsublatsum = Dlat + Mlat/60 + Slat/3600
+
+#longitude
+DMSsublongS = "%s" %tle_rec.sublong
+DMSsublong = DMSsublongS.split(':')
+Dlong = int(DMSsublong[0])
+Mlong = int(DMSsublong[1])
+Slong = int(float(DMSsublong[2]))
+DMSsublongsum = float(Dlong) + float(Mlong/60) + float(Slong/3600)
+
+
 print "Position: ", tle_rec.sublong, tle_rec.sublat
 
+print "\nDMSsublatsum: ", DMSsublatsum, DMSsublongsum
 
 print "\nAll done!"
